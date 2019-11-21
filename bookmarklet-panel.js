5d32529be364e0af2e897ece8644f4ec9763578f
Doug McKay <douglasamckay@gmail.com>
2019-11-20 20:59:30 -0800

    updated updates section with releases link

--- bookmarklet-panel.js
+++ bookmarklet-panel.js
@@ -62,7 +62,7 @@ add_bookmarklet('Remove Cookies', `javascript:void(document.cookie=null)`);
 
 // Updates Section
 add_label('Updates');
-add_bookmarklet('Bookmarklets', `https://github.com/weo-media/bookmarklets`);
+add_bookmarklet('Bookmarklets Releases', `https://github.com/weo-media/bookmarklets/releases`);
 add_bookmarklet('Close', hide_panel);
 
 // append bookmarklets

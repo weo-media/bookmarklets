97c062830dcb072e08678fdcd335c55600c0fb19
Doug McKay <douglasamckay@gmail.com>
2019-11-30 18:38:25 -0800

    removed version function. added link to sandbox.

--- bookmarklet-panel.js
+++ bookmarklet-panel.js
@@ -76,9 +76,8 @@ add_bookmarklet('Remove Cookies', `javascript:void(document.cookie=null)`);
 
 // Updates Section
 add_label('Latest Version');
-// add_Version();
 add_bookmarklet('<img src="https://img.shields.io/github/v/tag/weo-media/bookmarklets.svg">', 'https://github.com/weo-media/bookmarklets')
-add_bookmarklet('Bookmarklets Releases', `https://github.com/weo-media/bookmarklets/releases`);
+add_bookmarklet('Get Latest', `https://www.weo2.com/index.asp?N=dentist-Bookmarklets-Panel&Preview=1&C=777&P=43800`);
 add_bookmarklet('Close', remove_panel);
 
 // append bookmarklets
@@ -101,12 +100,6 @@ function hyperlink(label, url){
     a.href = url;
   }
   return a;
-}
-
-function add_Version() {
-  let imgVersion = create_tag('img');
-  imgVersion.src = "https://img.shields.io/github/v/tag/weo-media/bookmarklets.svg";
-  unorderedList.appendChild(imgVersion);
 }
 
 function add_bookmarklet(label, url){
